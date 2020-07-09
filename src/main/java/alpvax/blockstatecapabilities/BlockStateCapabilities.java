d6acaa137fe19d1cab3db15a0dc7451135406be0
Alpvax <development@alpvax.uk>
2020-07-09 17:30:17 +0100

    Fix posting events too early
    Events are now posted in loadcomplete, instead of setup, to ensure capabilities are registered

--- src/main/java/alpvax/blockstatecapabilities/BlockStateCapabilities.java
+++ src/main/java/alpvax/blockstatecapabilities/BlockStateCapabilities.java
@@ -4,12 +4,11 @@ import alpvax.blockstatecapabilities.api.IBlockCapabilitiesManager;
 import alpvax.blockstatecapabilities.api.RegisterBlockStateCapabilitiesEvent;
 import alpvax.blockstatecapabilities.vanilla.VanillaProviders;
 import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.items.IItemHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -26,15 +25,28 @@ public class BlockStateCapabilities
 
     public BlockStateCapabilities() {
         VanillaProviders.register();
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IBlockCapabilitiesManager manager = BlockStateCapabilitiesManager.INSTANCE;
+        modBus.addListener((FMLLoadCompleteEvent event) -> {
+            try {
+                Field f = CapabilityManager.class.getDeclaredField("providers");
+                f.setAccessible(true);
+                //noinspection unchecked
+                IdentityHashMap<String, Capability<?>> providers = (IdentityHashMap<String, Capability<?>>) f.get(CapabilityManager.INSTANCE);
+                providers.forEach((name, capability) -> postCapabilityEvent(modBus, manager, name, capability));
+            } catch (NoSuchFieldException e) {
+                LOGGER.error("Unable to hook into CapabilityManager callbacks (NoSuchMethod)", e);
+            } catch (IllegalAccessException e) {
+                LOGGER.error("Unable to hook into CapabilityManager callbacks (IllegalAccess)", e);
+            }
+        });
         /*TODO: Config
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ConfigOptions.SPEC);
         modBus.addListener(ConfigOptions::onModConfigEvent);
         */
     }
 
-    @SuppressWarnings("unchecked")
-    @CapabilityInject(IItemHandler.class)
-    private static void fireCapabilityEvents(Capability<IItemHandler> itemHandlerCapability) {
+    /*private void fireCapabilityEvents(Capability<IItemHandler> itemHandlerCapability) {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         IBlockCapabilitiesManager manager = BlockStateCapabilitiesManager.INSTANCE;
         try {
@@ -47,13 +59,13 @@ public class BlockStateCapabilities
         } catch (IllegalAccessException e) {
             LOGGER.error("Unable to hook into CapabilityManager callbacks (IllegalAccess)", e);
         }
-    }
+    }*/
 
     @SuppressWarnings("unchecked")
     private static <T> void postCapabilityEvent(IEventBus modBus, IBlockCapabilitiesManager manager, String name, Capability<T> capability) {
         try {
-            Class<T> type = (Class<T>) Class.forName(name.substring(1));
-            modBus.post(new RegisterBlockStateCapabilitiesEvent<T>(type, manager, capability));
+            Class<T> type = (Class<T>) Class.forName(name);
+            modBus.post(new RegisterBlockStateCapabilitiesEvent<>(type, manager, capability));
         } catch (ClassNotFoundException e) {
             LOGGER.error("Error finding Capability class: {} (ClassNotFound)", name.substring(1), e);
         }
